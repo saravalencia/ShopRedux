5bdac1dd5afef51fa20df58592d390d0abff5bc6
Sara Valencia <sara.valencia.m@gmail.com>
2021-06-28 15:51:53 +0200

    added all features

---
+++ src/containers/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="ui fixed menu">
+            <h2>Shop</h2>
+            <div className="ui container center">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Header 
